becfd7083e891fe7f61b2cc4a37da6d6c1f7aa3c
ali afshar <zahramasteali@gmail.com>
2020-07-24 00:37:19 +0430

    start socket.

--- src/main/java/ir/ac/kntu/cs2d/client/ClientMain.java
+++ src/main/java/ir/ac/kntu/cs2d/client/ClientMain.java
@@ -1,8 +1,25 @@
 package ir.ac.kntu.cs2d.client;
 
+import ir.ac.kntu.ScannerSingleton;
+import ir.ac.kntu.gamelogic.CTPlayer;
+import ir.ac.kntu.gamelogic.Player;
+import ir.ac.kntu.gamelogic.TPlayer;
+
+import java.io.IOException;
+import java.net.Socket;
+
 public class ClientMain {
-    public static void main(String[] args) {
-        // TODO
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost",8192);
+        ScannerSingleton scannerSingleton = ScannerSingleton.getInstance();
+        Player player;
+        System.out.println("press 1 for t and 2 for ct:");
+        String choice = scannerSingleton.nextLine();
+        if (choice.equals("1")){
+            player = new TPlayer(0, 0);
+        } else {
+            player = new CTPlayer(100, 100);
+        }
     }
 
     private ClientMain() {
--- src/main/java/ir/ac/kntu/cs2d/server/ServerMain.java
+++ src/main/java/ir/ac/kntu/cs2d/server/ServerMain.java
@@ -1,8 +1,45 @@
 package ir.ac.kntu.cs2d.server;
 
+import ir.ac.kntu.ScannerSingleton;
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerMain {
-    public static void main(String[] args) {
-        // TODO
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(8192);
+        System.out.println("hello,type 1 for start:");
+        ScannerSingleton scannerSingleton = ScannerSingleton.getInstance();
+        if (!scannerSingleton.nextLine().equals("1")){
+            serverSocket.close();
+            return;
+        }
+        new Thread(()->{
+            Socket socket;
+            do {
+                try {
+                    socket = serverSocket.accept();
+                    clientHandler(socket);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } while (!serverSocket.isClosed());
+        },"Client Handler").start();
+        System.out.println("enter any thing to close server:");
+        scannerSingleton.nextLine();
+        serverSocket.close();
+    }
+
+    private static void clientHandler(Socket socket) throws IOException {
+
     }
 
     private ServerMain() {
