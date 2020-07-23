e0c4ae81772982ad95bce2b26a515a339f7b1c40
ali afshar <zahramasteali@gmail.com>
2020-07-23 20:40:46 +0430

    check style test cases.

--- src/main/java/ir/ac/kntu/gamelogic/PPBizon.java
+++ src/main/java/ir/ac/kntu/gamelogic/PPBizon.java
@@ -1,7 +1,7 @@
 package ir.ac.kntu.gamelogic;
 
-public class PP_Bizon extends MainGun{
-    public PP_Bizon() {
+public class PPBizon extends MainGun{
+    public PPBizon() {
         super(1400, 27, 100, 2.4, 64, true);
     }
 }
